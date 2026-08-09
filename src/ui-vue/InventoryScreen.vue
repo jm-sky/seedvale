2cfbbfe81e9cb584b7751099f1533bd941a5b92d
Jan Madeyski <jan.madeyski@gmail.com>
2026-08-09 20:57:06 +0200

    Refactor inventory screen layout and improve HTML structure for better readability

--- src/ui-vue/InventoryScreen.vue
+++ src/ui-vue/InventoryScreen.vue
@@ -49,16 +49,32 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div v-show="ui.inventory.open" class="seedvale-inventory" @click.self="closeInventory">
-    <div ref="panel" class="seedvale-inventory__panel">
+  <div
+    v-show="ui.inventory.open"
+    class="seedvale-inventory"
+    @click.self="closeInventory"
+  >
+    <div
+      ref="panel"
+      class="seedvale-inventory__panel"
+    >
       <h1>Ekwipunek</h1>
       <div class="seedvale-inventory__weight">
         Waga: {{ formatWeight(ui.inventory.totalWeight) }} / {{ formatWeight(ui.inventory.maxWeight) }}
       </div>
 
       <div class="seedvale-inventory__list">
-        <div v-if="items.length === 0" class="seedvale-inventory__empty">Ekwipunek jest pusty.</div>
-        <div v-for="item in items" :key="item.kind" class="seedvale-inventory__row">
+        <div
+          v-if="items.length === 0"
+          class="seedvale-inventory__empty"
+        >
+          Ekwipunek jest pusty.
+        </div>
+        <div
+          v-for="item in items"
+          :key="item.kind"
+          class="seedvale-inventory__row"
+        >
           <div class="seedvale-inventory__row-main">
             <span class="seedvale-inventory__row-name">{{ item.count }} × {{ item.def.label }}</span>
             <span class="seedvale-inventory__row-category">{{ categoryLabel[item.def.category] }}</span>
@@ -66,7 +82,13 @@ onBeforeUnmount(() => {
           <div class="seedvale-inventory__row-weight">
             {{ formatWeight(item.def.weight) }} szt. · {{ formatWeight(item.def.weight * item.count) }} razem
           </div>
-          <button type="button" class="seedvale-inventory__drop" @click="onDrop(item.kind)">Wyrzuć</button>
+          <button
+            type="button"
+            class="seedvale-inventory__drop"
+            @click="onDrop(item.kind)"
+          >
+            Wyrzuć
+          </button>
         </div>
       </div>
 
