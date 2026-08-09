3fdeeb9a509cec744adad9d3e2de4519c0a6a011
Jan Madeyski <jan.madeyski@gmail.com>
2026-08-09 19:58:51 +0200

    refactor(ui): render inventory screen in Vue root

--- src/ui-vue/App.vue
+++ src/ui-vue/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import InventoryScreen from './InventoryScreen.vue'
 import NpcDialogueMenu from './NpcDialogueMenu.vue'
 </script>
 
 <template>
   <div class="pointer-events-none fixed inset-0 z-10">
     <NpcDialogueMenu />
+    <InventoryScreen />
   </div>
 </template>
